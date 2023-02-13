07cec5acacc3c77b1951b1058139fb9e108deb73
sairam <sairamsreejith0@gmail.com>
2023-02-14 04:59:42 +0530

    initial change

---
+++ WaterfallModel/calc.py
@@ -0,0 +1,9 @@
+def addition(value1,value2):
+    return value1+value2
+
+def subtraction(value1,value2):
+    return value1-value2 
+
+def multiplication(value1,value2):
+    return value1*value2 
+
