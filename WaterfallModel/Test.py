481b8a0f87898e8df766d09f3921f87bf7130b3a
sairam <sairamsreejith0@gmail.com>
2023-02-14 11:56:23 +0530

    complete

---
+++ WaterfallModel/Test.py
@@ -0,0 +1,36 @@
+import unittest
+from calc import Calculator
+class AdditionTest(unittest.TestCase):
+    def test_add_positiveNumber(self):
+        self.actual = Calculator.addition(3,5)
+        self.expected = 8
+        self.assertEqual(self.actual,self.expected)
+    def test_add_negativeNumber(self):
+        self.actual = Calculator.addition(-4,-8)
+        self.expected = -12        
+        self.assertEqual(self.actual,self.expected)
+
+class SubtractionTest(unittest.TestCase):
+    def test_sub_positiveNumber(self):
+        self.actual = Calculator.subtraction(5,4)
+        self.expected = 1
+        self.assertEqual(self.actual,self.expected)
+    def test_sub_negativeNumber(self):
+        self.actual = Calculator.subtraction(-4,-8)
+        self.expected = 4
+        self.assertEqual(self.actual,self.expected)
+        
+class MultiplicationTest(unittest.TestCase):
+    def test_multiply_positiveNumber(self):
+        self.actual = Calculator.multiplication(5,4)        
+        self.expected = 20 
+        self.assertEqual(self.actual,self.expected)
+    def test_multiply_negativeNumber(self):
+        self.actual = Calculator.multiplication(-4,-4)        
+        self.expected = 16 
+        self.assertEqual(self.actual,self.expected)    
+            
+            
+
+    
+
--- WaterfallModel/calc.py
+++ WaterfallModel/calc.py
@@ -1,9 +1,43 @@
-def addition(value1,value2):
-    return value1+value2
+class Calculator:
+    def addition(value1,value2):
+        return value1+value2
 
-def subtraction(value1,value2):
-    return value1-value2 
+    def subtraction(value1,value2):
+        return value1-value2 
 
-def multiplication(value1,value2):
-    return value1*value2 
+    def multiplication(value1,value2):
+        return value1*value2 
 
+if __name__ == '__main__':
+    while(True):
+        print("menu")
+        print("1.addition")
+        print("2.subtraction")
+        print("3.multiplication")
+        print("4.Exit")
+        print("choose a number(1,2,3 or 4)")
+        choice = int(input())
+        if choice==1:
+            print("enter a number")
+            number1 = int(input())
+            print("enter a number")
+            number2 = int(input())
+            print('addition of {} and {} is {}'.format(number1,number2,Calculator.addition(number1,number2)))
+        elif choice==2:
+            print("enter a number")
+            number1 = int(input())
+            print("enter a number")
+            number2 = int(input())
+            print('subtraction of {} and {} is {}'.format(number1,number2,Calculator.subtraction(number1,number2)))
+        elif choice==3:
+            print("enter a number")
+            number1 = int(input())
+            print("enter a number")
+            number2 = int(input())
+            print('multiplication of {} and {} is {}'.format(number1,number2,Calculator.multiplication(number1,number2)))
+        elif choice==4:
+            break
+        else:
+            pass        
+                
+            
